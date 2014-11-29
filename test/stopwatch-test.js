1b4ab6d17b9d6140ed59f82b0a9c0576dc437496
Jason Ray <jason.ray@agilex.com>
2014-11-29 09:30:08 -0500

    move Stopwatch to lib

--- test/stopwatch-test.js
+++ test/stopwatch-test.js
@@ -1,7 +1,7 @@
 /*jslint node: true */
 "use strict";
 
-var Stopwatch = require('../Stopwatch');
+var Stopwatch = require('../lib/Stopwatch');
 var defaultPrecision = 5;
 
 exports.initStopwatch = function(test) {
