fc01062bf5be2ae6c3cb5e4f862fc7e20136f504
Jason Ray <jasonray@jayray.net>
2017-01-07 13:26:56 -0500

    migrating stopwatch back to child project
    drop duplicate test

--- test/stopwatch-test-2.js
+++ test/stopwatch-test-2.js
@@ -10,25 +10,6 @@ var should = require('should');
 describe('stopwatch 2', function () {
     this.timeout(5000);
 
-    it.skip('executing stop twice should return time at second stop', function (done) {
-        //start(), wait .1s, stop(), wait .2s, stop(), wait .5s, read(), ensure delta = .3s
-        var stopwatch = new Stopwatch();
-        stopwatch.start();
-        setTimeout(function () {
-            stopwatch.stop();
-
-            setTimeout(function () {
-                TestHelper.assertCloseEnough(100 + 200, stopwatch.stop(), defaultPrecision);
-
-                setTimeout(function () {
-                    var delta = stopwatch.read();
-                    TestHelper.assertCloseEnough(100 + 200, delta, defaultPrecision);
-                    done();
-                }, 500);
-            }, 200);
-        }, 100);
-    });
-
     it.skip('ensure that stop() returns time', function (done) {
         var testtime = 100;
         var stopwatch = new Stopwatch();
