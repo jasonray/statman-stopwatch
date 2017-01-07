50ef7a190693fcae79f1529aa0a6936aedde5e5e
Jason Ray <jasonray@jayray.net>
2017-01-07 13:18:03 -0500

    migrating stopwatch back to child project
    add should for assertions

--- test/stopwatch-test.js
+++ test/stopwatch-test.js
@@ -5,6 +5,8 @@ var Stopwatch = require('../lib/Stopwatch');
 var defaultPrecision = 15;
 var mocha = require('mocha');
 var assert = require('assert');
+var should = require('should');
+
 
 describe('stopwatch', function () {
     this.timeout(5000);
