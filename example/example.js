1773f756fa43644bc67fc31138c097af49419109
Jason Ray <jayray.net@gmail.com>
2023-10-20 23:13:26 -0400

    cleanup code warnings
    Codacy prefers “ vs ‘

--- example/example.js
+++ example/example.js
@@ -8,30 +8,30 @@ function expensiveOperation(numberOfTimes) {
 // const Stopwatch = require('statman-stopwatch');
 const Stopwatch = require('../lib/Stopwatch');
 
-console.log('starting sample project');
+console.log("starting sample project");
 
 // example #1, initialize the sw, start it, do an expensive operation, read sw
 const sw1 = new Stopwatch();
 sw1.start();
-console.log('starting first expensive operation');
+console.log("starting first expensive operation");
 expensiveOperation(1000000);
-console.log('expensive operation time #1: ', sw1.read());
+console.log("expensive operation time #1: ", sw1.read());
 
 // example #2, initialize the sw with autostart, do an expensive operation, read sw
 const sw2 = new Stopwatch(true);
-console.log('starting second expensive operation');
+console.log("starting second expensive operation");
 expensiveOperation(1000000);
-console.log('expensive operation time #2: ', sw2.read());
+console.log("expensive operation time #2: ", sw2.read());
 
 // example #3, initialize the sw with autostart, do an expensive operation, stop sw, read sw
 const sw3 = new Stopwatch(true);
-console.log('starting third expensive operation');
+console.log("starting third expensive operation");
 expensiveOperation(1000000);
 sw3.stop();
-console.log('expensive operation time #3: ', sw3.read());
+console.log("expensive operation time #3: ", sw3.read());
 
 // example #4, initialize the sw with autostart, do an expensive operation, stop sw
 const sw4 = new Stopwatch(true);
-console.log('starting fourth expensive operation');
+console.log("starting fourth expensive operation");
 expensiveOperation(1000000);
-console.log('expensive operation time #4: ', sw4.stop());
+console.log("expensive operation time #4: ", sw4.stop());
